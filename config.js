5603b5f7619b1f28138803ea5e1ded7d5f6eeeb9
vima-bot1 <gaveshvimanshana1234@gmail.com>
2026-02-18 21:43:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/VIMA-MD/VIMA-MD/blob/main/images/VIMA-MD.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIMA-MD Is Alive Now😍*",
+BOT_OWNER: '94742549935',  // Replace with the owner's phone number
 
 
 
